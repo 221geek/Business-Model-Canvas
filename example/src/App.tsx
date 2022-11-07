ca8b8dd741a41a942b260feb016a2b8941783535
Abdoul Aziz Dione <abdoulazizdione1997@gmail.com>
2022-11-07 19:19:01 +0000

    gitignore

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-
-import { ExampleComponent } from 'business-model-canvas'
+import { BusinessModelCanvas } from 'business-model-canvas'
 import 'business-model-canvas/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <BusinessModelCanvas text="Create React Library Example 😄" />
 }
 
 export default App
